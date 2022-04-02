7c6cb9bfecdfb36fd37e9a2cc2706ecacc85132f
kokila2000 <77538586+kokila2000@users.noreply.github.com>
2022-04-02 22:22:48 +0530

    Add files via upload

---
+++ spring/remotehiring/src/main/java/com/remotehiring/RemotehiringApplication.java
@@ -0,0 +1,14 @@
+package com.remotehiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//main application
+@SpringBootApplication
+public class RemotehiringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemotehiringApplication.class, args);
+	}
+
+}
